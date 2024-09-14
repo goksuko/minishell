0167b297a4ec902264fce23d9f44327623bd3715
Vanessa-BV <vanessa.busekrus@gmail.com>
2024-09-14 18:01:48 +0200

    added handling of 18752 to be replaced with PID and if there is a string after $ that is no env, such as  - in this case get_env will return an empty string

--- sources/argument_expansion/dollar_sign.c
+++ sources/argument_expansion/dollar_sign.c
@@ -63,29 +63,8 @@ int	get_end_dollar(char *str, int i)
 	return (i - 1);
 }
 
-// int	get_len_to_skip(char *str)
-// {
-// 	printf("---REPLACE DOLLAR WITH ENV VALUE---\n");
-// 	printf("str: %s\n", str); // only for testing
-// 	int		len;
-// 	char	*temp;
-
-// 	temp = malloc((sizeof(char) * 1) + env_len(str));
-// 	if (temp == NULL)
-// 	{
-// 		// free_shell_data(shell_data); // to be written 
-// 		// exit code and string to print
-// 	}
-// 	temp = ft_strcpy(temp, str, env_len(str));
-// 	printf("temp: %s\n", temp); // only for testing
-// 	len = ft_strlen(temp);
-// 	printf("len: %d\n", len); // only for testing
-// 	return (len);
-// }
-
 char	*get_env_str(char *str) // need to add shell_data struct
 {
-	printf("---REPLACE DOLLAR WITH ENV VALUE---\n");
 	char	*temp;
 	char	*env;
 
@@ -96,14 +75,9 @@ char	*get_env_str(char *str) // need to add shell_data struct
 		// exit code and string to print
 	}
 	temp = ft_strcpy(temp, str, env_len(str));
-	printf("temp: %s\n", temp); // only for testing
 	env = getenv(temp);
 	if (env == NULL)
-	{
-		// free_shell_data(shell_data); // to be written 
-		// exit code and string to print
-	}
-	printf("env: %s\n", env); // only for testing
+		return (ft_strdup(""));
 	return (env);
 }
 
@@ -112,47 +86,73 @@ char	*handle_dollar_sign(t_data **shell_data, char *str) // need to add shell_da
 	int		i;
 	int		start_dollar;
 	int		end_dollar;
-	char	*start;
+	char	*temp;
 	char	*middle_to_add;
 
 	i = 0;
 	start_dollar = 0;
 	end_dollar = 0;
-	start = ft_strdup("");
-	//error handling
+	temp = ft_strdup("");
+	if (temp == NULL)
+	{
+		// free_shell_data(shell_data); // to be written
+		// exit code and string to print
+	}
 	while (str[i] != '\0')
 	{
 		if (str[i] == '$')
 		{
-			printf("starting string: %s\n", start); // only for testing‚
 			start_dollar = i;
-			if (str[i + 1] == '?') //  && str[i - 1] != '$' // check for correctness
+			if (str[i + 1] == '?')
 			{
 				middle_to_add = ft_itoa((*shell_data)->exit_code);
-				end_dollar = start_dollar + 2;
+				if (middle_to_add == NULL)
+				{
+					// free_shell_data(shell_data); // to be written
+					// exit code and string to print
+				}
+				end_dollar = start_dollar + 1;
+			}
+			else if (str[i + 1] == '$')
+			{
+				middle_to_add = ft_itoa(getpid());
+				if (middle_to_add == NULL)
+				{
+					// free_shell_data(shell_data); // to be written
+					// exit code and string to print
+				}
+				end_dollar = start_dollar + 1;
 			}
 			else if (str[i + 1] != '\0' && (ft_isalnum(str[i + 1]) == 1 || str[i + 1] == ' '))
 			{
-				printf("start_dollar: %d\n",start_dollar); // only for testing
-				printf("char at start_dollar: %c\n",str[start_dollar]); // only for testing
-				printf("end_dollar: %d\n",end_dollar); // only for testing
-				printf("char at end_dollar: %c\n",str[end_dollar]); // only for testing
 				middle_to_add = get_env_str(str + i + 1);
+				if (middle_to_add == NULL)
+				{
+					// free_shell_data(shell_data); // to be written
+					// exit code and string to print
+				}
 				end_dollar = get_end_dollar(str, i + 1); // + 1 because i is the index of the dollar sign
 			}
-			printf("middle_to_add: %s\n", middle_to_add); // only for testing
-			start = ft_strjoin(start, middle_to_add);
-			// error handling
-			// free middle to_add?
+			temp = ft_strjoin(temp, middle_to_add);
+			if (temp == NULL)
+			{
+				// free_shell_data(shell_data); // to be written
+				// exit code and string to print
+			}
 			i = end_dollar + 1; // Jump to end_dollar
 		}
 		else
 		{
-			start = ft_strjoin_c(start, str[i]);
+			temp = ft_strjoin_c(temp, str[i]);
+			if (temp == NULL)
+			{
+				// free_shell_data(shell_data); // to be written
+				// exit code and string to print
+			}
 			i++;
 		}
 	}
-	return (start);
+	return (temp);
 }
 
 bool	dollar_sign_check(char *str)
