69a74e2b473d312c8021788c946b39ae3dd1dec1
GoksuKO <goksuk@gmail.com>
2024-09-24 22:44:20 +0200

    adding studies done in Codam today

--- sources/semantic_analysis/semantic.c
+++ sources/semantic_analysis/semantic.c
@@ -7,13 +7,14 @@ void semantic_analysis(t_data *shell_data)
 	printf("---SEMANTIC ANALYSIS---\n");
 	shell_data->exit_code = 0;
 	shell_data->nbr_of_pipes = find_pipe_count(shell_data->line);
-	printf("nbr_of_pipes: %d\n", shell_data->nbr_of_pipes);
+	// printf("nbr_of_pipes: %d\n", shell_data->nbr_of_pipes);
 	info = (t_pipex *)ft_calloc(1, sizeof(t_pipex));
 	if (info == NULL || errno == ENOMEM)
 		ft_exit_data_perror(shell_data, ERROR_ALLOCATION, "info in pipex");
 	shell_data->info = info;
 	info->nbr_of_cmds = shell_data->nbr_of_pipes + 1;
-	shell_data->cmds = shell_data->ast->argument;
+	// shell_data->cmds = shell_data->ast->argument;
+	shell_data->cmds = shell_data->expanded_cmds;
 	info->cmds = shell_data->cmds;
 	initialize_cmds(shell_data, info);
 	initialize_info(info, shell_data);
