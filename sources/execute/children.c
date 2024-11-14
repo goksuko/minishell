e562fa6f29974e61d4adfc61ebd88dca9e4b6ff8
goksu <akaya-oz@student.codam.nl>
2024-11-14 13:23:27 +0100

    solved echo hello > out problem

--- sources/execute/children.c
+++ sources/execute/children.c
@@ -6,7 +6,7 @@
 /*   By: akaya-oz <akaya-oz@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/10/19 22:59:07 by akaya-oz      #+#    #+#                 */
-/*   Updated: 2024/11/13 12:20:50 by akaya-oz      ########   odam.nl         */
+/*   Updated: 2024/11/14 13:03:56 by akaya-oz      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,16 +63,22 @@ bool	do_last_child(t_info *info)
 	{
 		if (dup2(info->fd_out, STDOUT_FILENO) < 0)
 			return (false);
+		ms_close(info->data, info->fd_out);
+		info->fd_out = -10;
 	}
 	if (info->fd_in != -10)
 	{
 		if (dup2(info->fd_in, STDIN_FILENO) < 0)
 			return (false);
+		ms_close(info->data, info->fd_in);
+		info->fd_in = -10;
 	}
 	else if (info->curr_cmd != 0)
 	{
 		if (dup2(info->pipe_read_end, STDIN_FILENO) < 0)
 			return (false);
+		ms_close(info->data, info->pipe_read_end);
+		info->pipe_read_end = STDIN_FILENO;
 	}
 	return (true);
 }
